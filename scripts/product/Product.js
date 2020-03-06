6dbcc83e1e26dd1096028defc2c2f7f565a05fb0
Keartroth <mecarrolljr@gmail.com>
2020-03-06 14:38:15 -0600

    added product and productList to dynamically render product to the DOM.

---
+++ scripts/product/Product.js
@@ -0,0 +1,9 @@
+export const product = (productObject) => {
+    return`
+    <section id="product--${productObject.id}" class="product">
+        <h4>${productObject.name}</h4>
+        <h6>${productObject.beanType}</h6>
+        <p>Roast: ${productObject.roastType}    Price: $${productObject.price}</p>
+    </section>
+    `
+}
---
+++ scripts/product/ProductList.js
@@ -0,0 +1,12 @@
+import { useProducts } from "./productDataProvider.js";
+import { product } from "./Product.js";
+
+const contentElement = document.querySelector(".container--products");
+
+export const productList = () => {
+    const arrayOfProductObjects = useProducts();
+
+    for (const productObject of arrayOfProductObjects) {
+        contentElement.innerHTML += product(productObject);
+    }
+}
